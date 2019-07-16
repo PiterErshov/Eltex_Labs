2ec2ad6641e52cecb2972511b8539ff75e6b6248
Piter <PMI62.Ershov@outlook.com>
2019-07-16 11:44:10 +0700

    Fixed problem with fork, added file with prototyps.

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -1,17 +1,8 @@
-/*
- ============================================================================
- Name        : lab6.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include "f.h"
@@ -22,10 +13,9 @@ int main(int argc, char *argv[])
 {
 
 	FILE *fr;
-	char s1[1024];
+	char s1[N];
 	char **ch = NULL;
 	int s = 0;
-
 	if((fr=fopen(argv[1],"a+"))==NULL)
 	{
 		printf("Невозможно открыть файл для чтения.\n");
@@ -33,14 +23,29 @@ int main(int argc, char *argv[])
 	}
 
 	strcpy(s1, argv[2]);
-
-
-	ch = readf(fr, &s);
-	search(ch, &s, s1);
+	
+	//printf("%s", s1);
+	
+	ch = readf(fr, &s);		
+  	fclose(fr);
+  	
+	ch = Del_end(ch, s);
+	
+	fork_search(ch, s, s1);
+	
+	if((fr=fopen("buf.txt","a+"))==NULL)
+	{
+		printf("Невозможно открыть файл для чтения.\n");
+		exit(1);
+	}
+	wait(0);
+	char b[N];
+	fgets(b, 100, fr);
+	int m = atoi(b);
+	printf("Слово %s найдено %d раз \n", s1, m);
 //	outf(fu, ch, buf, &s);
 	freeMas(ch);
 
-  	fclose(fr);
 
 	return 0;
 }
