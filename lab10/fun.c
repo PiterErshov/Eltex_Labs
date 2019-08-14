3f36c46f5a77c8841f3557a67c8c203f718f6ac3
Piter <PMI62.Ershov@outlook.com>
2019-08-14 14:23:20 +0700

    Problem with synchronization

--- lab10/fun.c
+++ lab10/fun.c
@@ -4,8 +4,11 @@ void *fighter(void *arg)
 {
 	int i = *(int*)arg, res, pt = 0;
 	void *status;
-	/*pthread_mutex_lock(&put.mutex[i]);
-	/*if (i == put.max - 1)
+	
+	printf("Thread %d ready\n", i);
+	pthread_mutex_lock(&put.mutex[i]);
+	
+	if (i == put.max - 1)
 		for (int j = 0; j < put.max; j++)
 			pthread_mutex_unlock(&put.mutex[j]);
 	//*/
@@ -15,34 +18,19 @@ void *fighter(void *arg)
 		srand(i * time(NULL));
 		srand(rand());
 		pt = rand() % put.max;
-		if (pt != i && live(pt))
+		if (pt != i && put.kill[pt] == 0)
 			break;
 	}
-	printf("Thread %d ready\n", i);
 	printf("Thread %d kill thread %d\n", i, pt);
-	res = pthread_join(put.threads[pt], &status);
-	if (res != 0)
-		return EXIT_FAILURE;
-			
-
+	pthread_cancel(put.threads[pt]);
 	put.kill[pt] = 1;
-	int j = 0;
+	/*int j = 0;
 	while(j < put.max)
 	{	
 		printf("THREAD %d :KILLED[%d] = %d\n", i, j, put.kill[j]);
 		fflush (stdout);
 		j++;
-	}
+	}*/
 	put.w = i;
 	pthread_exit(NULL);
-}
-
-int live(int pt)
-{
-	int f = 1;
-
-	if (put.kill[pt] == 0)
-		return 1;
-	else
-		return 0;
 }
